634598e694217033ecd1a0b31e491c0e32b565e7
VexTheVex <131091039+VexTheVex@users.noreply.github.com>
2023-06-11 15:07:30 -0500

    Create logins.lua

---
+++ logins.lua
@@ -0,0 +1,9 @@
+Pinfo.rex.id = "010"
+Pinfo.rex.pass = "1225"
+Pinfo.rex.perm = "admin"
+Pinfo.rex.bal = 1000
+
+Pinfo.billy.id = "020"
+Pinfo.billy.pass = "392326"
+Pinfo.billy.perm = "admin"
+Pinfo.rex.bal = 50
